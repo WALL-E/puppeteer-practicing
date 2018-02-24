6bdbc2393f67d8a248f5fc12d8c0e65a4edeb82e
zhangzheng <zzgigi2003@163.com>
2018-02-24 18:40:29 +0800

    增加退出功能

--- lab/login.js
+++ lab/login.js
@@ -1,20 +1,22 @@
 #!/usr/local/bin/node
 
 const puppeteer = require('puppeteer');
+const baseUrl = 'http://new-resthub.qianbao.com/';
 
 (async () => {
     const browser = await puppeteer.launch({
         executablePath: '/Applications/Chromium.app/Contents/MacOS/Chromium',
         headless: false,
         slowMo:10,
-        ignoreHTTPSErrors: true
+        ignoreHTTPSErrors: true,
+        args: ['--start-fullscreen']
     });
     const page = await browser.newPage();
     await page.setViewport({
     	width: 1920,
     	height: 1080
     })
-    await page.goto('http://new-resthub.qianbao.com/');
+    await page.goto(baseUrl);
     await page.waitFor('input[name=username]')
     await page.waitFor('input[name=password]')
     let name = await page.$('input[name=username]')
@@ -24,5 +26,16 @@ const puppeteer = require('puppeteer');
 
     const inputElement = await page.$('button[type=button]');
     await inputElement.click();
-    // browser.close();
+
+    await page.waitFor(3000);
+    console.log("page url: " + page._target._targetInfo.url)
+
+    const accountMenu = await page.$('#account-menu')
+    accountMenu.click()
+    await page.waitFor(1000);
+    const accountLogout = await page.$('#account-logout')
+    accountLogout.click()
+
+    await page.waitFor(1000);
+    browser.close();
 })();
